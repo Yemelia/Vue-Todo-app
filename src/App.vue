e53bbcc0d42d52cc8fd598693240be18baec08a5
Yemelia <yemelianovnk@gmail.com>
2018-01-29 00:53:48 +0300

    add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
     <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
@@ -17,18 +16,22 @@ components: {
   data() {
     return {
       todos: [{
+        id: 1,
         title: 'Todo A',
         project: 'Project A',
         done: false,
       }, {
+        id: 2,
         title: 'Todo B',
         project: 'Project B',
         done: true,
       }, {
+        id:3,
         title: 'Todo C',
         project: 'Project C',
         done: false,
       }, {
+        id:4,
         title: 'Todo D',
         project: 'Project D',
         done: false,
