2afa9da93d7e7cf549638883a7e87b8f7a88aa43
Yemelia <yemelianovnk@gmail.com>
2018-01-29 00:42:40 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+export default {
+  name: 'app',
+components: {
+    TodoList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
