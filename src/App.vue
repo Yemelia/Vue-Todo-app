a165e421c93da73d6737e0ae9bc95be63f6e8016
Nikita Yemelianov <yemelianovnk@gmail.com>
2018-01-30 11:49:13 +0300

    add adding tasks + change status

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo
   },
-  // data function avails data to the template
   data() {
     return {
       todos: [{
-        id: 1,
         title: 'Todo A',
         project: 'Project A',
         done: false,
       }, {
-        id: 2,
         title: 'Todo B',
         project: 'Project B',
         done: true,
       }, {
-        id:3,
         title: 'Todo C',
         project: 'Project C',
         done: false,
       }, {
-        id:4,
         title: 'Todo D',
         project: 'Project D',
         done: false,
       }],
     };
+  },
+  methods:{
+    addTodo(todo){
+      this.todos.push(todo);
+    }
   }
 }
 </script>
